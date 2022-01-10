043270a84260256f61b7d58b2c7e1e08c11022dc
William Daniel da Silva Kuhs <wdarchenemy@gmail.com>
2022-01-10 07:59:00 -0300

    Initial Commit

---
+++ SwiftUI-Videos/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-Videos
+//
+//  Created by William Daniel da Silva Kuhs on 10/01/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI-Videos/SwiftUI_VideosApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_VideosApp.swift
+//  SwiftUI-Videos
+//
+//  Created by William Daniel da Silva Kuhs on 10/01/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_VideosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
